9d844194f9177936eeefc3e6a07f27710e3badf4
ddialar <ddialar@gmail.com>
2019-08-30 21:34:44 +0100

    chore(modules.database): Define the set of data.

---
+++ src/modules/database/data/directors.ts
@@ -0,0 +1,38 @@
+export default [
+    {
+      "id": 1,
+      "name": "Morten Tyldum",
+      "birthDate": "1967-05-19",
+      "country": "Norway"
+    },
+    {
+      "id": 2,
+      "name": "Scott Derrickson",
+      "birthDate": "1966-03-18",
+      "country": "United States"
+    },
+    {
+      "id": 3,
+      "name": "Ridley Scott",
+      "birthDate": "1937-11-30",
+      "country": "United Kingdom"
+    },
+    {
+      "id": 4,
+      "name": "Christopher Nolan",
+      "birthDate": "1970-06-30",
+      "country": "United Kingdom"
+    },
+    {
+      "id": 5,
+      "name": "Lana Wachowski",
+      "birthDate": "1965-06-21",
+      "country": "United States"
+    },
+    {
+      "id": 6,
+      "name": "Lilly Wachowski",
+      "birthDate": "1967-12-29",
+      "country": "United States"
+    }
+]
---
+++ src/modules/database/data/users.ts
@@ -0,0 +1,10 @@
+export default [
+    {
+      "id": 1,
+      "name": "John",
+      "surname": "Doe",
+      "username": "jdoe",
+      "password": "123456",
+      "token": ""
+    }
+]
