4c6d68d93ee8eaded861d79ff88a3a6feacc24d3
ddialar <ddialar@gmail.com>
2019-08-31 08:59:31 +0100

    fix(modules.graphql.models.movie): Fix error on 'rating' field name.

--- src/modules/graphql/models/movie/movie.resolvers.ts
+++ src/modules/graphql/models/movie/movie.resolvers.ts
@@ -3,7 +3,7 @@ import * as ports         from '@ports';
 
 export default {
     Query: {
-        movies: (parentValue: any, args: any, context: any): MovieInterface[] => {
+        movies: (parentValue: any, args: any, context: any, astData: any): MovieInterface[] => {
             return ports.getMovies(args.movieIds || []);
         }
     }
