f3a0cc53aeded6cab6bd6c21291a6f9ca5270c94
ddialar <ddialar@gmail.com>
2019-09-01 07:17:38 +0100

    feat(modules.database.orm.users.requests): Implement the 'updateUserToken' method and its testing suite. All tests in green.

--- src/modules/database/orm/users.requests.ts
+++ src/modules/database/orm/users.requests.ts
@@ -1,12 +1,32 @@
 import { UserInterface } from '@interfaces';
 import users             from '@database/data/users';
 
+// // ###############################################################
+// // ##########            READING OPERATIONS             ##########
+// // ###############################################################
+
 const getUserByUsername = (username: string): UserInterface | null => {
     let selectedUser = users.find(user => user.username === username);
     return (selectedUser) ? selectedUser : null;
 };
 
+// // ###############################################################
+// // ##########           UPDATING OPERATIONS             ##########
+// // ###############################################################
+
+const updateUserToken = (userId: number, newToken: string): UserInterface | null => {
+    // TODO: Check if the token is valida (not empty string).
+    let selectedUser = users.find(user => {
+        if (user.id === userId) {
+            user.token = newToken;
+            return user;
+        }
+    });
+    return (selectedUser) ? selectedUser : null;
+};
+
 export {
-    getUserByUsername
+    getUserByUsername,
+    updateUserToken
 };
 
