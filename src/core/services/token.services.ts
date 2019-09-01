49d27a003681adb17f2121ab51f77ec88e0930a4
ddialar <ddialar@gmail.com>
2019-09-01 22:56:05 +0100

    refactor(core.services.token.services): Replace the 'jwt-simple' library by the 'jsonwebtoken' one.

--- src/core/services/token.services.ts
+++ src/core/services/token.services.ts
@@ -1,28 +1,28 @@
 import logger   from '@logger';
 
-import * as jwt from 'jwt-simple';
+import * as jwt from 'jsonwebtoken';
 
 interface JwtPayloadInterface {
     sub: string
-    iat: number
-    exp: number
+};
+interface JwtOptionsInterface {
+    algorithm: string
+    expiresIn: number
 };
 
 const encodeToken = (username: string): string => {
     try {
-        // validate.users.username(username, 'The user name must be defined in order to encode the token.');
-        // validate.users.userRoleValue(role, 'The user role must be defined in order to encode the token.');
-
         logger.trace('(encodeJwt) - Encoding JWT token ...');
 
-        let encodedToken: string = '';
         let payload: JwtPayloadInterface = {
-            sub: username,
-            iat: new Date().getTime(),
-            exp: (new Date().getTime()) + 60000
+            sub: username
         }
-
-        encodedToken = jwt.encode(payload, process.env.JWT_KEY || '');
+        let secret: string = process.env.JWT_KEY!;
+        let options: JwtOptionsInterface = {
+            algorithm: process.env.JWT_ALGORITHM || 'HS512',
+            expiresIn: parseInt(process.env.JWT_EXPIRATION_TIME_SECONDS!) || 60
+        };
+        let encodedToken: string =  jwt.sign(payload, secret, options);
     
         logger.trace('(encodeJwt) - JWT token successfully encoded.');
         return encodedToken;
@@ -32,43 +32,21 @@ const encodeToken = (username: string): string => {
     }
 };
 
-// const decodeJwt = (userToken) => {
-//     try {
-//         validate.users.userToken(userToken, 'The user token must be defined in order to decode it.');
-        
-//         logger.trace('(decodeJwt) - Decoding JWT token ...');
+const decodeToken = (userToken: string) => {
+    try {
+        logger.trace('(verifyJwt) - Decoding JWT token ...');
 
-//         let decodedToken = jwt.decode(userToken, authorizationConf.jwt.key);
-//         logger.info('(dencodeJwt) - JWT token successfully decoded.');
-//         return decodedToken;
-//     } catch (error) {
-//         logger.error(`(decodeJwt) - ${error.message} ${error.description}`);
-//         throw new ApiError.UnauthorizedTokenInvalid((error.description) ? error.description : error.message);
-//     }
-// };
+        let decodedToken = jwt.verify(userToken, process.env.JWT_KEY!);
 
-// const validateToken = (userToken) => {
-//     try {
-//         validate.users.userToken(userToken, 'The user token must be defined in order to validate it.');
-
-//         logger.trace('(validateToken) - Validatting JWT token ...');
-
-//         let tokenDecoded;
-        
-//         try { 
-//             tokenDecoded = decodeJwt(userToken);
-//         } catch (error) {
-//             throw new ApiError.UnauthorizedTokenExpired('The token is out of date.');
-//         }
-
-//         logger.info('(validateToken) - JWT token successfully validated.');
-//         return tokenDecoded;
-//     } catch (error) {
-//         logger.error(`(validateToken) - ${error.message} ${error.description}`);
-//         throw new ApiError.RuntimeError(error.code, error.message, error.description);
-//     }
-// };
+        logger.info('(dencodeJwt) - JWT token successfully decoded.');
+        return decodedToken;
+    } catch (error) {
+        logger.error(`(verifyJwt) - ${error.message} ${error.description}`);
+        throw new Error ('Token not valid.');
+    }
+};
 
 export {
-    encodeToken
+    encodeToken,
+    decodeToken
 };
