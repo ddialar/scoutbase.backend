385f9225ddb442899a614fb4bcddbb6c84219c8d
ddialar <ddialar@gmail.com>
2019-09-07 21:29:29 +0100

    tests(adapters.director.adapters): Adapt the 'getDirectorsByMovieId' testing suite to the ORM refactor. All tests in green.

---
+++ tests/adapters/director.adapters/getDirectorsByMovieId.test.ts
@@ -0,0 +1,82 @@
+import * as dotenv from 'dotenv';
+import * as path   from 'path';
+dotenv.config({ path: path.join(process.env.PWD, '/config/.env/test.env') });
+
+import orm                       from '@orm';
+import { DirectorInterface }     from '@interfaces';
+import { getDirectorsByMovieId } from '@adapters';
+
+const mockedMovieId: number = 6;
+const mockedDirectorsData: DirectorInterface[] = [
+    {
+        "id": 5,
+        "name": "Lana Wachowski",
+        "birthday": "1965-06-21",
+        "country": "United States"
+    },
+    {
+        "id": 6,
+        "name": "Lilly Wachowski",
+        "birthday": "1967-12-29",
+        "country": "United States"
+    }
+];
+
+beforeAll(async (done) => {
+    try {
+        await orm.connect();
+    } catch (error) {
+        console.log(`[ERROR] (getDirectorsByMovieId) - Connecting the database: ${error.message}`);
+    } finally {
+        done();
+    }
+});
+
+afterAll(async (done) => {
+    try {
+        await orm.disconnect();
+    } catch (error) {
+        console.log(`[ERROR] (getDirectorsByMovieId) - Disconnecting the database: ${error.message}`);
+    } finally {
+        done();
+    }
+});
+
+describe('Testing Director adapters ...', () => {
+    describe('working with \'getDirectorsByMovieId\' ...', () => {
+        test('with no movie ID provided, it must return an empty collection.', () => {
+            let movieId = null;
+
+            expect(getDirectorsByMovieId(movieId)).rejects.toThrow('Movie identification not valid.');
+        });
+        test('providing a non recorded movie ID, it must return an empty collection.', async (done) => {
+            let movieId = 99;
+            let expectedResult = [];
+            let obtainedResult = await getDirectorsByMovieId(movieId);
+
+            expect(obtainedResult).toHaveLength(expectedResult.length);
+
+            done();
+        });
+        test('providing a movie ID, it must return all directors bound with this movie.', async (done) => {
+            let movieId = mockedMovieId;
+            let expectedResult = mockedDirectorsData;
+            let obtainedResult = await getDirectorsByMovieId(movieId);
+
+            expect(obtainedResult).not.toBeNull();
+            expect(obtainedResult).toHaveLength(expectedResult.length)
+            obtainedResult.map((obtainedDirector, index) => {
+                expect(obtainedDirector).toHaveProperty('id');
+                expect(obtainedDirector.id).toBe(expectedResult[index].id);
+                expect(obtainedDirector).toHaveProperty('name');
+                expect(obtainedDirector.name).toBe(expectedResult[index].name);
+                expect(obtainedDirector).toHaveProperty('birthday');
+                expect(obtainedDirector.birthday).toBe(expectedResult[index].birthday);
+                expect(obtainedDirector).toHaveProperty('country');
+                expect(obtainedDirector.country).toBe(expectedResult[index].country);
+            });
+
+            done();
+        });
+    });
+});
