27d4408c670a62a8d2f8269cbe2978342a4c8a57
ddialar <ddialar@gmail.com>
2019-09-07 19:39:17 +0100

    tests(modules.typeorm.controllers.directors.controllers): Type the 'getDirectorsByActorId' and 'getDirectorsByMovieId' testing suites. All tests in green.

--- tests/modules/typeorm/controllers/directors.controllers/getDirectorsByActorId.test.ts
+++ tests/modules/typeorm/controllers/directors.controllers/getDirectorsByActorId.test.ts
@@ -1,39 +1,70 @@
-import directors             from '@database/data/directors';
-import { directorsRequests } from '@database/orm';
+import * as dotenv from 'dotenv';
+import * as path   from 'path';
+dotenv.config({ path: path.join(process.env.PWD, '/config/.env/test.env') });
+
+import orm                   from '@orm';
+import { DirectorInterface } from '@interfaces';
+
+const mockedActorId: number = 1;
+const mockedDirectorsData: DirectorInterface[]= [
+    {
+        "id": 1,
+        "name": "Morten Tyldum",
+        "birthday": "1967-05-19",
+        "country": "Norway"
+    },
+    {
+        "id": 2,
+        "name": "Scott Derrickson",
+        "birthday": "1966-03-18",
+        "country": "United States"
+    }
+];
+
+beforeAll(async (done) => {
+    try {
+        await orm.connect();
+    } catch (error) {
+        console.log(`[ERROR] (getDirectorsByActorId) - Connecting the database: ${error.message}`);
+    } finally {
+        done();
+    }
+});
+
+afterAll(async (done) => {
+    try {
+        await orm.disconnect();
+    } catch (error) {
+        console.log(`[ERROR] (getDirectorsByActorId) - Disconnecting the database: ${error.message}`);
+    } finally {
+        done();
+    }
+});
 
 describe('Testing ORM ...', () => {
     describe('working with \'getDirectorsByActorId\' ...', () => {
-        test('with no actor ID provided, it must return an empty collection.', () => {
+        test('with no actor ID provided, it must return an empty collection.', async (done) => {
             let actorId = null;
             let expectedResult = [];
-            let obtainedResult = directorsRequests.getDirectorsByActorId(actorId);
+            let obtainedResult = await orm.getDirectorsByActorId(actorId);
 
             expect(obtainedResult).toHaveLength(expectedResult.length);
+
+            done();
         });
-        test('providing a non recorded actor ID, it must return an empty collection.', () => {
+        test('providing a non recorded actor ID, it must return an empty collection.', async (done) => {
             let actorId = 99;
             let expectedResult = [];
-            let obtainedResult = directorsRequests.getDirectorsByActorId(actorId);
+            let obtainedResult = await orm.getDirectorsByActorId(actorId);
 
             expect(obtainedResult).toHaveLength(expectedResult.length);
+
+            done();
         });
-        test('providing a actor ID, it must return all directors bound with this actor.', () => {
-            let actorId = 1;
-            let expectedResult = [
-                {
-                    "id": 1,
-                    "name": "Morten Tyldum",
-                    "birthday": "1967-05-19",
-                    "country": "Norway"
-                },
-                {
-                    "id": 2,
-                    "name": "Scott Derrickson",
-                    "birthday": "1966-03-18",
-                    "country": "United States"
-                }
-            ];
-            let obtainedResult = directorsRequests.getDirectorsByActorId(actorId);
+        test('providing a actor ID, it must return all directors bound with this actor.', async (done) => {
+            let actorId = mockedActorId;
+            let expectedResult = mockedDirectorsData;
+            let obtainedResult = await orm.getDirectorsByActorId(actorId);
 
             expect(obtainedResult).not.toBeNull();
             expect(obtainedResult).toHaveLength(expectedResult.length)
@@ -47,6 +78,8 @@ describe('Testing ORM ...', () => {
                 expect(obtainedDirector).toHaveProperty('country');
                 expect(obtainedDirector.country).toBe(expectedResult[index].country);
             });
+
+            done();
         });
     });
 });
