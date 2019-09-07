7b9f4f24130b6b3a14854d9e06e60aecaa4f42e5
ddialar <ddialar@gmail.com>
2019-09-07 19:40:14 +0100

    tests(modules.typeorm.controllers.actors.controllers): Type the 'getActorsByMovieId' testing suite. All tests in green.

--- tests/modules/typeorm/controllers/actors.controllers/getActorsByMovieId.test.ts
+++ tests/modules/typeorm/controllers/actors.controllers/getActorsByMovieId.test.ts
@@ -1,58 +1,89 @@
-import actors             from '@database/data/actors';
-import { actorsRequests } from '@database/orm';
+import * as dotenv from 'dotenv';
+import * as path   from 'path';
+dotenv.config({ path: path.join(process.env.PWD, '/config/.env/test.env') });
+
+import orm                from '@orm';
+import { ActorInterface } from '@interfaces';
+
+const mockedActorId: number = 2;
+const mockedActorsData: ActorInterface[] = [
+    {
+        "id": 1,
+        "name": "Benedict Cumberbatch",
+        "birthday": "1976-06-19",
+        "country": "United Kingdom"
+    },
+    {
+        "id": 2,
+        "name": "Chiwetel Ejiofor",
+        "birthday": "1977-07-10",
+        "country": "United Kingdom"
+    },
+    {
+        "id": 3,
+        "name": "Rachel McAdams",
+        "birthday": "1978-11-17",
+        "country": "Canada"
+    },
+    {
+        "id": 4,
+        "name": "Benedict Wong",
+        "birthday": "1971-06-03",
+        "country": "United Kingdom"
+    },
+    {
+        "id": 5,
+        "name": "Tilda Swinton",
+        "birthday": "1960-11-05",
+        "country": "United Kingdom"
+    }
+];
+
+beforeAll(async (done) => {
+    try {
+        await orm.connect();
+    } catch (error) {
+        console.log(`[ERROR] (getActorsByMovieId) - Connecting the database: ${error.message}`);
+    } finally {
+        done();
+    }
+});
+
+afterAll(async (done) => {
+    try {
+        await orm.disconnect();
+    } catch (error) {
+        console.log(`[ERROR] (getActorsByMovieId) - Disconnecting the database: ${error.message}`);
+    } finally {
+        done();
+    }
+});
 
 describe('Testing ORM ...', () => {
     describe('working with \'getActorsByMovieId\' ...', () => {
-        test('with no movie ID provided, it must return an empty collection.', () => {
+        test('with no movie ID provided, it must return an empty collection.', async (done) => {
             let movieId = null;
             let expectedResult = [];
-            let obtainedResult = actorsRequests.getActorsByMovieId(movieId);
+            let obtainedResult = await orm.getActorsByMovieId(movieId);
 
             expect(obtainedResult).toHaveLength(expectedResult.length);
+
+            done();
         });
-        test('providing a non recorded movie ID, it must return an empty collection.', () => {
+        test('providing a non recorded movie ID, it must return an empty collection.', async (done) => {
             let movieId = 99;
             let expectedResult = [];
-            let obtainedResult = actorsRequests.getActorsByMovieId(movieId);
+            let obtainedResult = await orm.getActorsByMovieId(movieId);
 
             expect(obtainedResult).toHaveLength(expectedResult.length);
+
+            done();
         });
-        test('providing a movie ID, it must return all actors bound with this movie.', () => {
-            let movieId = 2;
-            let expectedResult = [
-                {
-                  "id": 1,
-                  "name": "Benedict Cumberbatch",
-                  "birthday": "1976-06-19",
-                  "country": "United Kingdom"
-                },
-                {
-                  "id": 2,
-                  "name": "Chiwetel Ejiofor",
-                  "birthday": "1977-07-10",
-                  "country": "United Kingdom"
-                },
-                {
-                  "id": 3,
-                  "name": "Rachel McAdams",
-                  "birthday": "1978-11-17",
-                  "country": "Canada"
-                },
-                {
-                  "id": 4,
-                  "name": "Benedict Wong",
-                  "birthday": "1971-06-03",
-                  "country": "United Kingdom"
-                },
-                {
-                  "id": 5,
-                  "name": "Tilda Swinton",
-                  "birthday": "1960-11-05",
-                  "country": "United Kingdom"
-                }
-            ];
-            let obtainedResult = actorsRequests.getActorsByMovieId(movieId);
-            
+        test('providing a movie ID, it must return all actors bound with this movie.', async (done) => {
+            let movieId = mockedActorId;
+            let expectedResult = mockedActorsData;
+            let obtainedResult = await orm.getActorsByMovieId(movieId);
+
             expect(obtainedResult).not.toBeNull();
             expect(obtainedResult).toHaveLength(expectedResult.length)
             obtainedResult.map((obtainedActor, index) => {
@@ -65,6 +96,8 @@ describe('Testing ORM ...', () => {
                 expect(obtainedActor).toHaveProperty('country');
                 expect(obtainedActor.country).toBe(expectedResult[index].country);
             });
+
+            done();
         });
     });
 });
