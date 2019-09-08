baf88bcf8150fbfa89b6b1e4a3cc5a6478b3e07e
ddialar <ddialar@gmail.com>
2019-09-08 14:21:51 +0100

    refactor(modules.graphql.directives): Edit the 'isAuthenticatedUser' directive in order to return different messages depending on the user's token has been provided or whether it's valid or it doesn't.

--- src/modules/graphql/directives/isAuthenticatedUser/isAuthenticatedUser.resolver.ts
+++ src/modules/graphql/directives/isAuthenticatedUser/isAuthenticatedUser.resolver.ts
@@ -1,21 +1,27 @@
 import { SchemaDirectiveVisitor } from 'apollo-server';
-import { defaultFieldResolver }   from 'graphql';
+import { defaultFieldResolver } from 'graphql';
 
-import { decodeToken }            from '@services/token.services';
+import { decodeToken } from '@services/token.services';
 
 export default class IsAuthenticatedUserDirective extends SchemaDirectiveVisitor {
     visitFieldDefinition(field: any) {
         const { resolve = defaultFieldResolver } = field;
 
         field.resolve = async (...args: any[]) => {
-            let [{}, {}, context] = args;
+            let [{ }, { }, context] = args;
+            let result: string = '';
 
-            if (context && context.token && decodeToken(context.token)) {
-                return await resolve.apply(this, args);
-            } else {
-                return 'Login required.';
+            try {
+                if (context && context.token && decodeToken(context.token)) {
+                    result = await resolve.apply(this, args);
+                } else {
+                    result = 'Login required.';
+                }
+            } catch (error) {
+                result = error.message;
+            } finally {
+                return result;
             }
-
         }
     };
 };
